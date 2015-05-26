23c8297ae74de240e80f0af98d40e65a0716b435
Matt Brubeck <mbrubeck@limpet.net>
2015-05-26 09:43:31 -0700

    Generate Bidi_Class lookup table from Unicode data.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,19 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#[test]
-fn it_works() {
+mod tables;
+pub use tables::UNICODE_VERSION;
+pub use tables::bidi::{BidiClass, bidi_class};
+
+#[cfg(test)]
+mod test {
+    use super::bidi_class;
+    use super::BidiClass::*;
+
+    #[test]
+    fn test_bidi_class() {
+        assert_eq!(bidi_class('c'), L);
+        assert_eq!(bidi_class('\u{05D1}'), R);
+        assert_eq!(bidi_class('\u{0627}'), AL);
+    }
 }
