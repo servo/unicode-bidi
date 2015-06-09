c8d8fed2fde1698dc9aee6158c013c2ca637d08a
Matt Brubeck <mbrubeck@limpet.net>
2015-06-09 16:56:05 -0700

    Fully implement isolating run sequences

--- src/lib.rs
+++ src/lib.rs
@@ -414,6 +414,8 @@ mod prepare {
     /// An isolating run sequence is a maximal sequence of level runs such that for all level runs
     /// except the last one in the sequence, the last character of the run is an isolate initiator
     /// whose matching PDI is the first character of the next level run in the sequence.
+    ///
+    /// Note: This function does *not* return the sequences in order by their first characters.
     pub fn isolating_run_sequences(para_level: u8, initial_classes: &[BidiClass], levels: &[u8])
         -> Vec<IsolatingRunSequence>
     {
@@ -421,17 +423,45 @@ mod prepare {
 
         // Compute the set of isolating run sequences.
         // http://www.unicode.org/reports/tr9/#BD13
+
         let mut sequences = Vec::with_capacity(runs.len());
+
+        // When we encounter an isolate initiator, we push the current sequence onto the
+        // stack so we can resume it after the matching PDI.
+        let mut stack = vec![Vec::new()];
+
         for run in runs {
-            // TODO: Actually check for isolate initiators and matching PDIs.
-            // For now this is just a stub that puts each level run in a separate sequence.
-            sequences.push(vec![run.clone()]);
+            assert!(run.len() > 0);
+            assert!(stack.len() > 0);
+
+            let start_class = initial_classes[run.start];
+            let end_class = initial_classes[run.end - 1];
+
+            let mut sequence = if start_class == PDI && stack.len() > 1 {
+                // Continue a previous sequence interrupted by an isolate.
+                stack.pop().unwrap()
+            } else {
+                // Start a new sequence.
+                Vec::new()
+            };
+
+            sequence.push(run);
+
+            if matches!(end_class, RLI | LRI | FSI) {
+                // Resume this sequence after the isolate.
+                stack.push(sequence);
+            } else {
+                // This sequence is finished.
+                sequences.push(sequence);
+            }
         }
+        // Pop any remaning sequences off the stack.
+        sequences.extend(stack.into_iter().rev().filter(|seq| seq.len() > 0));
 
         // Determine the `sos` and `eos` class for each sequence.
         // http://www.unicode.org/reports/tr9/#X10
         return sequences.into_iter().map(|sequence| {
-            assert!(sequence.len() > 0);
+            assert!(!sequence.len() > 0);
             let start = sequence[0].start;
             let end = sequence[sequence.len() - 1].end;
 
@@ -463,9 +493,6 @@ mod prepare {
     }
 
     /// Finds the level runs in a paragraph.
-    ///
-    /// `levels[i]` and `classes[i]` must contain the explicit embedding level and the BidiClass,
-    /// respectively, of the char at byte index `i`.
     ///
     /// http://www.unicode.org/reports/tr9/#BD7
     fn level_runs(levels: &[u8], classes: &[BidiClass]) -> Vec<LevelRun> {
@@ -508,6 +535,20 @@ mod prepare {
     #[cfg(test)] #[test]
     fn test_level_runs() {
         assert_eq!(level_runs(&[0,0,0,1,1,2,0,0], &[L; 8]), &[0..3, 3..5, 5..6, 6..8]);
+    }
+
+    #[cfg(test)] #[test]
+    fn test_isolating_run_sequences() {
+        // Example 3 from http://www.unicode.org/reports/tr9/#BD13:
+
+        //              0  1    2   3    4  5  6  7    8   9   10
+        let classes = &[L, RLI, AL, LRI, L, R, L, PDI, AL, PDI, L];
+        let levels =  &[0, 0,   1,  1,   2, 3, 2, 1,   1,  0,   0];
+        let para_level = 0;
+
+        let sequences = isolating_run_sequences(para_level, classes, levels);
+        let runs: Vec<Vec<LevelRun>> = sequences.iter().map(|s| s.runs.clone()).collect();
+        assert_eq!(runs, vec![vec![4..5], vec![5..6], vec![6..7], vec![2..4, 7..9], vec![0..2, 9..11]]);
     }
 }
 
