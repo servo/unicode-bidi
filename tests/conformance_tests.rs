f8967cf2e9e4a12536d03524ef81f5bffb2a9877
Behnam Esfahbod <behnam@zwnj.org>
2017-05-16 16:41:36 -0500

    Add benchmark tests using UDHR text files
    The bench functions are put in two groups: `for_ltr_texts` and
    `for_bidi_texts`. This will allow us measure improvements for both
    all-LTR and bidirectional situations.
    
    Current results:
    ```
    test bench_bidi_info_new_and_reordered_for_bidi_texts ... bench: 9,711,485 ns/iter (+/- 917,086)
    test bench_bidi_info_new_and_reordered_for_ltr_texts  ... bench: 6,496,454 ns/iter (+/- 829,583)
    test bench_bidi_info_new_for_bidi_texts               ... bench: 9,813,185 ns/iter (+/- 1,180,446)
    test bench_bidi_info_new_for_ltr_texts                ... bench: 6,811,823 ns/iter (+/- 1,079,303)
    ```

--- tests/conformance_tests.rs
+++ tests/conformance_tests.rs
@@ -7,7 +7,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![cfg(test)]
+#![cfg(all(test, not(feature = "unstable")))]
 
 extern crate unicode_bidi;
 
