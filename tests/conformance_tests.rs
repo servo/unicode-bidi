d87eef6626fcabf264c4fa7cb1b7ed2df9d9a1c0
Manish Goregaokar <manishsmail@gmail.com>
2022-03-03 11:07:15 -0800

    update expectation

--- tests/conformance_tests.rs
+++ tests/conformance_tests.rs
@@ -139,7 +139,7 @@ fn gen_base_levels_for_base_tests(bitset: u8) -> Vec<Option<Level>> {
 
 
 #[test]
-#[should_panic(expected = "14558 test cases failed! (77141 passed)")]
+#[should_panic(expected = "14562 test cases failed! (77145 passed)")]
 fn test_character_conformance() {
     let test_data = include_str!("data/BidiCharacterTest.txt");
 
